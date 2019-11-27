db7db0bd16d66ae987afdb4a0a251bc34ab3d505
Thomas-AgM <57763543+Thomas-AgM@users.noreply.github.com>
2019-11-27 11:45:25 +0100

    Update simon.ino

--- Arduino/simon/simon.ino
+++ Arduino/simon/simon.ino
@@ -1,275 +1,14 @@
-
 #include <Adafruit_NeoPixel.h>;
-#include <stdio.h>
-#include <stdlib.h>
+/*#include <stdio.h>
+#include <stdlib.h>*/
 #include <time.h>
-
-
-
-
+#include <LEDMatrixDriver.hpp>
 
 
 #define BUZZER1  4
 #define BUZZER2  7
 
-//got
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 4978
- void GameOfThrones()
-  {
-    for(int i=0; i<4; i++)
-    {
-    tone(BUZZER1, NOTE_G4);
-    delay(500);
-    noTone(BUZZER1);
-    tone(BUZZER1, NOTE_C4);
-    delay(500);
-    noTone(BUZZER1);
-    tone(BUZZER1, NOTE_DS4);
-    delay(250);
-    noTone(BUZZER1);
-    tone(BUZZER1, NOTE_F4);
-    delay(250);
-    noTone(BUZZER1);
-    }
-    for(int i=0; i<4; i++)
-    {
-    tone(BUZZER1, NOTE_G4);
-    delay(500);
-    noTone(BUZZER1);
-    tone(BUZZER1, NOTE_C4);
-    delay(500);
-    noTone(BUZZER1);
-    tone(BUZZER1, NOTE_E4);
-    delay(250);
-    noTone(BUZZER1);
-    tone(BUZZER1, NOTE_F4);
-    delay(250);
-    noTone(BUZZER1);
-    }
-        tone(BUZZER1, NOTE_G4);
-        delay(500);
-        noTone(BUZZER1);
-        tone(BUZZER1, NOTE_C4);
-        delay(500);
-        tone(BUZZER1, NOTE_DS4);
-        delay(250);
-        noTone(BUZZER1);
-        tone(BUZZER1, NOTE_F4);
-        delay(250);
-        noTone(BUZZER1);
-        tone(BUZZER1, NOTE_D4);
-        delay(500);
-        noTone(BUZZER1);
-    for(int i=0; i<3; i++)
-    {
-    tone(BUZZER1, NOTE_G3);
-    delay(500);
-    noTone(BUZZER1);
-    tone(BUZZER1, NOTE_AS3);
-    delay(250);
-    noTone(BUZZER1);
-    tone(BUZZER1, NOTE_C4);
-    delay(250);
-    noTone(BUZZER1);
-    tone(BUZZER1, NOTE_D4);
-    delay(500);
-    noTone(BUZZER1);
-    }//
-        tone(BUZZER1, NOTE_G3);
-        delay(500);
-        noTone(BUZZER1);
-        tone(BUZZER1, NOTE_AS3);
-        delay(250);
-        noTone(BUZZER1);
-        tone(BUZZER1, NOTE_C4);
-        delay(250);
-        noTone(BUZZER1);
-        tone(BUZZER1, NOTE_D4);
-        delay(1000);
-        noTone(BUZZER1);
-         
-        tone(BUZZER1, NOTE_F4);
-        delay(1000);
-        noTone(BUZZER1);
-        tone(BUZZER1, NOTE_AS3);
-        delay(1000);
-        noTone(BUZZER1);
-        tone(BUZZER1, NOTE_DS4);
-        delay(250);
-        noTone(BUZZER1);
-        tone(BUZZER1, NOTE_D4);
-        delay(250);
-        noTone(BUZZER1);
-        tone(BUZZER1, NOTE_F4);
-        delay(1000);
-        noTone(BUZZER1);
-        tone(BUZZER1, NOTE_AS3);
-        delay(1000);
-        noTone(BUZZER1);
-        tone(BUZZER1, NOTE_DS4);
-        delay(250);
-        noTone(BUZZER1);
-        tone(BUZZER1, NOTE_D4);
-        delay(250);
-        noTone(BUZZER1);
-        tone(BUZZER1, NOTE_C4);
-        delay(500);
-        noTone(BUZZER1);
-    for(int i=0; i<3; i++)
-    {
-    tone(BUZZER1, NOTE_GS3);
-    delay(250);
-    noTone(BUZZER1);
-    tone(BUZZER1, NOTE_AS3);
-    delay(250);
-    noTone(BUZZER1);
-    tone(BUZZER1, NOTE_C4);
-    delay(500);
-    noTone(BUZZER1);
-    tone(BUZZER1, NOTE_F3);
-    delay(500);
-    noTone(BUZZER1);
-    }
-          tone(BUZZER1, NOTE_G4);
-          delay(1000);
-          noTone(BUZZER1);
-          tone(BUZZER1, NOTE_C4);
-          delay(1000);
-          noTone(BUZZER1);
-          tone(BUZZER1, NOTE_DS4);
-          delay(250);
-          noTone(BUZZER1);
-          tone(BUZZER1, NOTE_F4);
-          delay(250);
-          noTone(BUZZER1);
-          tone(BUZZER1, NOTE_G4);
-          delay(1000);
-          noTone(BUZZER1);
-          tone(BUZZER1, NOTE_C4);
-          delay(1000);
-          noTone(BUZZER1);
-          tone(BUZZER1, NOTE_DS4);
-          delay(250);
-          noTone(BUZZER1);
-          tone(BUZZER1, NOTE_F4);
-          delay(250);
-          noTone(BUZZER1);
-          tone(BUZZER1, NOTE_D4);
-          delay(500);
-          noTone(BUZZER1);
-    for(int i=0; i<4; i++)
-    {
-    tone(BUZZER1, NOTE_G3);
-    delay(500);
-    noTone(BUZZER1);
-    tone(BUZZER1, NOTE_AS3);
-    delay(250);
-    noTone(BUZZER1);
-    tone(BUZZER1, NOTE_C4);
-    delay(250);
-    noTone(BUZZER1);
-    tone(BUZZER1, NOTE_D4);
-    delay(500);
-    noTone(BUZZER1);
-    }
-}
-
-
-
-
-
-
-
-
-
+/*
 //noel
 const float note[12] = {65.41, 69.30, 73.42, 77.78, 82.41, 87.31, 92.50, 98.00, 103.83, 110.00, 116.54, 123.47
                        };
@@ -299,13 +38,13 @@ const int nombreDeNotes = 49;
   }
 }
   
-
+*/
 
 
 #define PIN 3
 #define PIN2 5
 #define PIN3 6
-#define PIN4 9
+#define PIN4 10
 
 
 
@@ -322,10 +61,10 @@ const int nombreDeNotes = 49;
 
 
 
-int sensorpin = 0;                 // analog pin used to connect the sharp sensor
-int sensorpin2 = 1;
-int sensorpin3 = 2;
-int sensorpin4 = 3;
+#define sensorpin  0                 // analog pin used to connect the sharp sensor
+#define sensorpin2  1
+#define sensorpin3  2
+#define sensorpin4  3
 
 
 
@@ -334,10 +73,22 @@ Adafruit_NeoPixel pixels2(NUMPIXELS2, PIN2, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixels3(NUMPIXELS3, PIN3, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixels4(NUMPIXELS4, PIN4, NEO_GRB + NEO_KHZ800);
 
-
+ //Matrix
+  const uint8_t LEDMATRIX_CS_PIN = 9;
+  const int LEDMATRIX_SEGMENTS = 8;
+  const int LEDMATRIX_WIDTH    = LEDMATRIX_SEGMENTS * 8;
+  LEDMatrixDriver lmd(LEDMATRIX_SEGMENTS, LEDMATRIX_CS_PIN);
+  char text[] = "** LED MATRIX DEMO! ** (1234567890) ++ \"ABCDEFGHIJKLMNOPQRSTUVWXYZ\" ++ <$%/=?'.@,> --";
+  //const int ANIM_DELAY = 30;
 
 void setup()
 {
+
+  //Matrix
+    lmd.setEnabled(true);
+    lmd.setIntensity(2); // 0 = low, 10 = high
+
+  //Jeu Simon
   #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
   clock_prescale_set(clock_div_1);
   #endif
@@ -355,94 +106,142 @@ void setup()
   pixels2.clear();pixels2.show();
   pixels3.clear();pixels3.show();
   pixels4.clear();pixels4.show();
-}
-
-// On suppose a<b
-int rand_a_b(int a, int b){
-  return rand()%(b-a) +a;
-}
-
-void change_couleur_des_led(int couleur){
-  switch(couleur){
-        case 1:  pixels.setPixelColor(0, pixels.Color(0, 0, 255));  pixels.show();tone(4,440,100); delay(500);pixels.clear();pixels.show();delay(500);break;//bleu
-        case 2:  pixels2.setPixelColor(0, pixels2.Color(0, 255, 0));  pixels2.show();tone(4,350,100);delay(500); pixels2.clear();pixels2.show();delay(500);break;//vert
-        case 3:  pixels3.setPixelColor(0, pixels3.Color(255, 0, 0));  pixels3.show();tone(4,500,100);delay(500); pixels3.clear();pixels3.show();delay(500);break;// rouge
-        case 4:  pixels4.setPixelColor(0, pixels4.Color(125, 0, 125));  pixels4.show();tone(4,250,100);delay(500);pixels4.clear();pixels4.show();delay(500);break;// mauve
-  }
-}
-
-void change_couleur_des_led_real_time(int couleur){
-  switch(couleur){
-        case 1:  pixels.setPixelColor(0, pixels.Color(0, 0, 255));  pixels.show();tone(4,440,100); break;//bleu
-        case 2:  pixels2.setPixelColor(0, pixels2.Color(0, 255, 0));  pixels2.show();tone(4,350,100);break;//vert
-        case 3:  pixels3.setPixelColor(0, pixels3.Color(255, 0, 0));  pixels3.show();tone(4,500,100); break;// rouge
-        case 4:  pixels4.setPixelColor(0, pixels4.Color(125, 0, 125));  pixels4.show();tone(4,250,100);break;// mauve
-        case 99: pixels.clear(); pixels.show();pixels2.clear(); pixels2.show();pixels3.clear(); pixels3.show();pixels4.clear(); pixels4.show();break;
-  }
-}
+  //Fin setup.
+ }
 
 
-
+ 
+//Simon
  int val = 0;
  int  val2 = 0;
  int  val3 =0;
  int  val4 = 0;
+
+ int i = 0;
+ int z = 0;
  
+
  int choix_utilisateur = 0;
 
  bool pret = false ;
 
+ String myString;
+
  int tab[13];
- int  TAILLE = 5;
- 
+ int  TAILLE = 4;
 
  int perdu = 0;
  unsigned long debut_partie;
  int iteration = 0;
  bool affiche_couleur = false;
  int nbr_jeux = 1;
- 
+
+
+  //Matrix
+  int x=0, y=0;   // start top left
+byte font[95][8] = { {0,0,0,0,0,0,0,0}, // SPACE
+                     {0x10,0x18,0x18,0x18,0x18,0x00,0x18,0x18}, // EXCL
+                     {0x28,0x28,0x08,0x00,0x00,0x00,0x00,0x00}, // QUOT
+                     {0x00,0x0a,0x7f,0x14,0x28,0xfe,0x50,0x00}, // #
+                     {0x10,0x38,0x54,0x70,0x1c,0x54,0x38,0x10}, // $
+                     {0x00,0x60,0x66,0x08,0x10,0x66,0x06,0x00}, // %
+                     {0,0,0,0,0,0,0,0}, // &
+                     {0x00,0x10,0x18,0x18,0x08,0x00,0x00,0x00}, // '
+                     {0x02,0x04,0x08,0x08,0x08,0x08,0x08,0x04}, // (
+                     {0x40,0x20,0x10,0x10,0x10,0x10,0x10,0x20}, // )
+                     {0x00,0x10,0x54,0x38,0x10,0x38,0x54,0x10}, // *
+                     {0x00,0x08,0x08,0x08,0x7f,0x08,0x08,0x08}, // +
+                     {0x00,0x00,0x00,0x00,0x00,0x18,0x18,0x08}, // COMMA
+                     {0x00,0x00,0x00,0x00,0x7e,0x00,0x00,0x00}, // -
+                     {0x00,0x00,0x00,0x00,0x00,0x00,0x06,0x06}, // DOT
+                     {0x00,0x04,0x04,0x08,0x10,0x20,0x40,0x40}, // /
+                     {0x00,0x38,0x44,0x4c,0x54,0x64,0x44,0x38}, // 0
+                     {0x04,0x0c,0x14,0x24,0x04,0x04,0x04,0x04}, // 1
+                     {0x00,0x30,0x48,0x04,0x04,0x38,0x40,0x7c}, // 2
+                     {0x00,0x38,0x04,0x04,0x18,0x04,0x44,0x38}, // 3
+                     {0x00,0x04,0x0c,0x14,0x24,0x7e,0x04,0x04}, // 4
+                     {0x00,0x7c,0x40,0x40,0x78,0x04,0x04,0x38}, // 5
+                     {0x00,0x38,0x40,0x40,0x78,0x44,0x44,0x38}, // 6
+                     {0x00,0x7c,0x04,0x04,0x08,0x08,0x10,0x10}, // 7
+                     {0x00,0x3c,0x44,0x44,0x38,0x44,0x44,0x78}, // 8
+                     {0x00,0x38,0x44,0x44,0x3c,0x04,0x04,0x78}, // 9
+                     {0x00,0x18,0x18,0x00,0x00,0x18,0x18,0x00}, // :
+                     {0x00,0x18,0x18,0x00,0x00,0x18,0x18,0x08}, // ;
+                     {0x00,0x10,0x20,0x40,0x80,0x40,0x20,0x10}, // <
+                     {0x00,0x00,0x7e,0x00,0x00,0xfc,0x00,0x00}, // =
+                     {0x00,0x08,0x04,0x02,0x01,0x02,0x04,0x08}, // >
+                     {0x00,0x38,0x44,0x04,0x08,0x10,0x00,0x10}, // ?
+                     {0x00,0x30,0x48,0xba,0xba,0x84,0x78,0x00}, // @
+                     {0x00,0x1c,0x22,0x42,0x42,0x7e,0x42,0x42}, // A
+                     {0x00,0x78,0x44,0x44,0x78,0x44,0x44,0x7c}, // B
+                     {0x00,0x3c,0x44,0x40,0x40,0x40,0x44,0x7c}, // C
+                     {0x00,0x7c,0x42,0x42,0x42,0x42,0x44,0x78}, // D
+                     {0x00,0x78,0x40,0x40,0x70,0x40,0x40,0x7c}, // E
+                     {0x00,0x7c,0x40,0x40,0x78,0x40,0x40,0x40}, // F
+                     {0x00,0x3c,0x40,0x40,0x5c,0x44,0x44,0x78}, // G
+                     {0x00,0x42,0x42,0x42,0x7e,0x42,0x42,0x42}, // H
+                     {0x00,0x7c,0x10,0x10,0x10,0x10,0x10,0x7e}, // I
+                     {0x00,0x7e,0x02,0x02,0x02,0x02,0x04,0x38}, // J
+                     {0x00,0x44,0x48,0x50,0x60,0x50,0x48,0x44}, // K
+                     {0x00,0x40,0x40,0x40,0x40,0x40,0x40,0x7c}, // L
+                     {0x00,0x82,0xc6,0xaa,0x92,0x82,0x82,0x82}, // M
+                     {0x00,0x42,0x42,0x62,0x52,0x4a,0x46,0x42}, // N
+                     {0x00,0x3c,0x42,0x42,0x42,0x42,0x44,0x38}, // O
+                     {0x00,0x78,0x44,0x44,0x48,0x70,0x40,0x40}, // P
+                     {0x00,0x3c,0x42,0x42,0x52,0x4a,0x44,0x3a}, // Q
+                     {0x00,0x78,0x44,0x44,0x78,0x50,0x48,0x44}, // R
+                     {0x00,0x38,0x40,0x40,0x38,0x04,0x04,0x78}, // S
+                     {0x00,0x7e,0x90,0x10,0x10,0x10,0x10,0x10}, // T
+                     {0x00,0x42,0x42,0x42,0x42,0x42,0x42,0x3e}, // U
+                     {0x00,0x42,0x42,0x42,0x42,0x44,0x28,0x10}, // V
+                     {0x80,0x82,0x82,0x92,0x92,0x92,0x94,0x78}, // W
+                     {0x00,0x42,0x42,0x24,0x18,0x24,0x42,0x42}, // X
+                     {0x00,0x44,0x44,0x28,0x10,0x10,0x10,0x10}, // Y
+                     {0x00,0x7c,0x04,0x08,0x7c,0x20,0x40,0xfe}, // Z
+                      // (the font does not contain any lower case letters. you can add your own.)
+                  };    // {}, //
 
 
  
-  
+ 
 void loop(){
-
+//Simon
   val = analogRead(sensorpin);
   val2 = analogRead(sensorpin2);
   val3 = analogRead(sensorpin3);
   val4 = analogRead(sensorpin4);
 
-    
-
-if (val>250 and val4>250){
+if (val>400 and val4>400){
   pret = true ;
   }else{
     pret = false;
   }
   
- 
-  
   if (pret){
-    Serial.println("Démarrage du jeux dans 5");
-    for (int i=0; i<5; i++){
-      Serial.println(5-i);
-      delay(1000);
+    affichage("START DANS");
+    delay(500);
+    for (i=0; i<4; i++){
+      myString = String(3-i);
+      if (i==3){
+         affichage("START");
+      }else{
+        affichage(myString);
+      }
+      delay(500);
     }
-    srand (time (NULL));
-    for(int i=0; i<TAILLE; i++){
-      tab[i] = rand_a_b(1,5);
-      Serial.print(tab[i]);
-      //change_couleur_des_led(tab[i]);
+
+    // tableau
+     for(int i =0; i<TAILLE; i++){
+        tab[i] = random(1, 5);
+   
     }
+
     
     while (nbr_jeux < TAILLE+1 and perdu == 0){
-      while (iteration < nbr_jeux and perdu == 0){  //1?
+      while (iteration < nbr_jeux and perdu == 0){ 
         if (affiche_couleur == false){
-          for (int z = 0; z < nbr_jeux; z++){
+          for (z = 0; z < nbr_jeux; z++){
             change_couleur_des_led(tab[z]);
-            Serial.println("allume le ");
-            Serial.print(tab[z]);
           }
           affiche_couleur = true;
           debut_partie = millis();
@@ -464,7 +263,6 @@ if (val>250 and val4>250){
           choix_utilisateur = 4;
           }
         if (choix_utilisateur == 1 || choix_utilisateur == 2 || choix_utilisateur == 3 || choix_utilisateur == 4){
-          Serial.println(choix_utilisateur);
           change_couleur_des_led_real_time(choix_utilisateur);
           if (choix_utilisateur == tab[iteration]){
             while (val >= 250 || val2 >= 250 || val3 >= 250 || val4 >= 250){
@@ -476,7 +274,6 @@ if (val>250 and val4>250){
             change_couleur_des_led_real_time(99);
             iteration++;
             debut_partie = millis();
-            Serial.println("gagné la couleur");
           }else{
             perdu = 1;
             change_couleur_des_led_real_time(99);
@@ -488,7 +285,6 @@ if (val>250 and val4>250){
           }
         }
       }
-      Serial.println("Jeu validé");
       affiche_couleur = false;
       iteration = 0;
       nbr_jeux++;
@@ -496,11 +292,10 @@ if (val>250 and val4>250){
     }
 
     if (perdu == 0){
-      Serial.println("Youhooooooo!!!!!");
-      jingleBells();
+      affichage("BIEN JOUE VOUS AVEZ GAGNE");
+      //jingleBells();
     }else{
-      Serial.println("Tu as perdu!!!");  
-       GameOfThrones();
+      affichage("VOUS AVEZ PERDU AU LEVEL"+ String(nbr_jeux-1));
     }
 
     nbr_jeux = 1;
@@ -509,5 +304,78 @@ if (val>250 and val4>250){
     pret = false;
  
    }
-  
 }
+
+
+
+//Simon
+void change_couleur_des_led_real_time(int couleur){
+  switch(couleur){
+        case 1:  pixels.setPixelColor(0, pixels.Color(0, 0, 255));  pixels.show();tone(4,440,100); break;//bleu
+        case 2:  pixels2.setPixelColor(0, pixels2.Color(0, 255, 0));  pixels2.show();tone(4,350,100);break;//vert
+        case 3:  pixels3.setPixelColor(0, pixels3.Color(255, 0, 0));  pixels3.show();tone(4,500,100); break;// rouge
+        case 4:  pixels4.setPixelColor(0, pixels4.Color(125, 0, 125));  pixels4.show();tone(4,250,100);break;// mauve
+        case 99: pixels.clear(); pixels.show();pixels2.clear(); pixels2.show();pixels3.clear(); pixels3.show();pixels4.clear(); pixels4.show();break;
+  }
+}
+void change_couleur_des_led(int couleur){
+  switch(couleur){
+        case 1:  pixels.setPixelColor(0, pixels.Color(0, 0, 255));  pixels.show();tone(4,440,100); delay(500);pixels.clear();pixels.show();delay(500);break;//bleu
+        case 2:  pixels2.setPixelColor(0, pixels2.Color(0, 255, 0));  pixels2.show();tone(4,350,100);delay(500); pixels2.clear();pixels2.show();delay(500);break;//vert
+        case 3:  pixels3.setPixelColor(0, pixels3.Color(255, 0, 0));  pixels3.show();tone(4,500,100);delay(500); pixels3.clear();pixels3.show();delay(500);break;// rouge
+        case 4:  pixels4.setPixelColor(0, pixels4.Color(125, 0, 125));  pixels4.show();tone(4,250,100);delay(500);pixels4.clear();pixels4.show();delay(500);break;// mauve
+  }
+}
+
+
+//Matrix
+
+
+
+void drawString(char* text, int len, int x, int y )
+{
+  for( int idx = 0; idx < len; idx ++ )
+  {
+    int c = text[idx] - 32;
+    if( x + idx * 8  > LEDMATRIX_WIDTH )
+      return;
+
+    if( 8 + x + idx * 8 > 0 )
+      drawSprite( font[c], x + idx * 8, y, 8, 8 );
+  }
+}
+
+void drawSprite( byte* sprite, int x, int y, int width, int height )
+{
+
+  byte mask = B10000000;
+
+  for( int iy = 0; iy < height; iy++ )
+  {
+    for( int ix = 0; ix < width; ix++ )
+    {
+      lmd.setPixel(x + ix, y + iy, (bool)(sprite[iy] & mask ));
+
+      mask = mask >> 1;
+    }
+    mask = B10000000;
+  }
+}
+
+
+void affichage(String mot){
+  Serial.print(mot);
+  char text[mot.length()+1];
+  mot.toCharArray(text, mot.length()+1);
+  x=63;
+  int len = strlen(text);
+
+  for (int i=0; i<((len*8)+64); i++){
+    drawString(text, len, x, 0);
+    lmd.display();
+    delay(30);
+    if( --x < len * -8 ) {
+      x = LEDMATRIX_WIDTH;
+      }
+    } 
+  }
